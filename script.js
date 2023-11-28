e08014f34fe5c8be6b771274fc01bc754175ddd2
rugbyprof <terry.griffin@msutexas.edu>
2023-11-28 07:26:16 -0600

    fourth

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("added listener")
+    document.getElementById('myButton').addEventListener('click', updateContent);
+});
+
 function updateContent() {
     document.getElementById('dynamic-content').innerHTML = 'This content was updated by JavaScript!';
 }
